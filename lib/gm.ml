a749a0663d1794e8df17bc9d8b1af4c30c6f4b6a
zaijian <czy175898@alibaba-inc.com>
2018-12-12 21:29:29 +0800

    pretty print gm

--- lib/gm.ml
+++ lib/gm.ml
@@ -1,4 +1,5 @@
 open Syntax
+open Pp
 
 (* aux *)
 let rec drop l n =
@@ -126,14 +127,26 @@ let slide n state =
   putStack (a :: drop stack n) state
 
 let unwind state =
-  let stack = getStack state in
+  let a, stack =
+    match getStack state with
+    | a :: stack -> (a, stack)
+    | _ -> failwith "unwind"
+  in
+  let heap = getHeap state in
   let newstate node =
     match node with
-    | NNum n -> putStack [] state
-    | NAp (a1, a2) -> putStack (a1 :: stack) state
-    | NGlobal (n, c) -> failwith ""
+    | NNum _ -> putStack [] state
+    | NAp (a1, _a2) ->
+        let code = getCode state in
+        putStack (a1 :: a :: stack) state |> putCode (Unwind :: code)
+    | NGlobal (n, c) ->
+        if List.length stack < n - 1 then
+          failwith
+            (Printf.sprintf "not enough arg, %d, %d" (List.length stack) n)
+        else putCode c state
   in
-  failwith ""
+  let node = Heap_hashtbl.find heap a in
+  newstate node
 
 let dispatch instr state =
   match instr with
@@ -143,3 +156,160 @@ let dispatch instr state =
   | Mkap -> mkap state
   | Slide n -> slide n state
   | Unwind -> unwind state
+
+let isFinal state = match getCode state with [] -> true | _ -> false
+
+let step state =
+  let i, is =
+    match getCode state with i :: is -> (i, is) | _ -> failwith "step"
+  in
+  dispatch i (putCode is state)
+
+let do_admin s = s
+
+let rec eval state =
+  let rest_state =
+    if isFinal state then [] else eval (do_admin (step state))
+  in
+  state :: rest_state
+
+(* compiler follows *)
+
+type gm_env = int Global_map.t
+
+type gm_compiler = core_expr -> gm_env -> gmCode
+
+type gm_compile_sc = name * int * gmCode
+
+let arg_offset (env : gm_env) n = Global_map.map (fun v -> v + n) env
+
+let rec compileC : gm_compiler =
+ fun expr env ->
+  match expr with
+  | EVar v -> (
+    match Global_map.find_opt v env with
+    | None -> [Push_global v]
+    | Some n -> [Push n] )
+  | ENum n -> [Push_int n]
+  | EAp (e1, e2) ->
+      List.append
+        (List.append (compileC e2 env) (compileC e1 (arg_offset env 1)))
+        [Mkap]
+  | _ -> failwith "compileC: not yet"
+
+let compileR expr env =
+  let count = Global_map.cardinal env in
+  List.append (compileC expr env) [Slide (count + 1); Unwind]
+
+let compileSC ((name, env, body) : core_sc_defn) : gm_compile_sc =
+  let env' =
+    List.combine env (List.init (List.length env) (fun i -> i))
+    |> List.to_seq |> Global_map.of_seq
+  in
+  (name, List.length env, compileR body env')
+
+let allocGlobal heap (sc : gm_compile_sc) =
+  let name, i, code = sc in
+  let heap', addr = halloc heap (NGlobal (i, code)) in
+  (heap', name, addr)
+
+let buildInitHeap (prog : core_program) : gmHeap * gmGlobals =
+  let compiled = List.map (fun def -> compileSC def) prog in
+  let initheap = Heap_hashtbl.create 100 in
+  let initglobals = Global_map.empty in
+  List.fold_left
+    (fun (heap, globals) e ->
+      let heap', name, addr = allocGlobal heap e in
+      let globals' = Global_map.add name addr globals in
+      (heap', globals') )
+    (initheap, initglobals) compiled
+
+let compile (prog : core_program) : gmState =
+  let initialCode = [Push_global "main"; Unwind] in
+  let initialStack = [] in
+  let initialHeap, initialGlobals =
+    buildInitHeap (List.append Syntax.prelude_defs prog)
+  in
+  let initStat = 0 in
+  (initialCode, initialStack, initialHeap, initialGlobals, initStat)
+
+(* =================================== show ============================= *)
+
+let show_instr instr =
+  match instr with
+  | Unwind -> istr "unwind"
+  | Push_global name -> iappend (istr "pushglobal ") (istr name)
+  | Push_int i -> iappend (istr "pushint ") (inum i)
+  | Push i -> iappend (istr "push ") (inum i)
+  | Mkap -> istr "mkap"
+  | Slide i -> iappend (istr "slide ") (inum i)
+
+let show_instrs code =
+  iconcat
+    [ istr "  code: {"
+    ; iindent
+        (iappend inewline (iinterleave inewline (List.map show_instr code)))
+    ; inewline
+    ; istr "        }"
+    ; inewline ]
+
+let show_sc s (name, addr) =
+  match Heap_hashtbl.find (getHeap s) addr with
+  | NNum _ | NAp _ -> failwith "show_sc"
+  | NGlobal (i, code) ->
+      iconcat
+        [ istr "code for :"
+        ; istr name
+        ; inewline
+        ; show_instrs code
+        ; inewline
+        ; inewline ]
+
+let show_addr addr = Printf.sprintf "%d" addr
+
+let show_node state addr n =
+  match n with
+  | NNum i -> inum i
+  | NAp (a1, a2) ->
+      iconcat [istr "ap "; istr (show_addr a1); istr " "; istr (show_addr a2)]
+  | NGlobal (i, code) ->
+      let (Some name) =
+        Global_map.fold
+          (fun name v r ->
+            match r with
+            | Some r' -> r
+            | None when addr = v -> Some name
+            | _ -> None )
+          (getGlobals state) None
+      in
+      iconcat [istr "Global "; istr name]
+
+let show_stack_item state addr =
+  let heap = getHeap state in
+  iconcat
+    [ istr (show_addr addr)
+    ; istr ": "
+    ; show_node state addr (Heap_hashtbl.find heap addr) ]
+
+let show_stack state =
+  iinterleave inewline (List.map (show_stack_item state) (getStack state))
+
+let show_state state =
+  iconcat [show_stack state; inewline; show_instrs (getCode state); inewline]
+
+let show_result states =
+  let (s :: _) = states in
+  idisplay
+    (iconcat
+       [ istr "supercombinator definitions"
+       ; inewline
+       ; iinterleave inewline
+           (Global_map.fold
+              (fun name addr r -> show_sc s (name, addr) :: r)
+              (getGlobals s) [])
+       ; inewline
+       ; inewline
+       ; istr "state trans"
+       ; inewline
+       ; inewline
+       ; ilayn (List.map show_state states) ])
